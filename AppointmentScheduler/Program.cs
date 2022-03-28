1b86446517ff5f0b5cdcc26966d6530356fbcfc5
Cosmin Ilincuta <ilincuta.cosmin@gmail.com>
2022-03-28 17:04:26 +0300

    Added partial View with tag helpers and configured custom User for Identity

--- AppointmentScheduler/Program.cs
+++ AppointmentScheduler/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
