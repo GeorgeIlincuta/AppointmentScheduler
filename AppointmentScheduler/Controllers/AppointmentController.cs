3d836211c7270f305c95a6d38b9d089fdb9dde03
Cosmin Ilincuta <ilincuta.cosmin@gmail.com>
2022-04-05 08:52:02 +0300

    added fullcalendar to admin login page

--- AppointmentScheduler/Controllers/AppointmentController.cs
+++ AppointmentScheduler/Controllers/AppointmentController.cs
@@ -13,7 +13,7 @@ namespace AppointmentScheduler.Controllers
         }
         public IActionResult Index()
         {
-            _appointmentService.GetDoctorList();
+            ViewBag.DoctorList = _appointmentService.GetDoctorList();
             return View();
         }
     }
--- AppointmentScheduler/Services/AppointmentService.cs
+++ AppointmentScheduler/Services/AppointmentService.cs
@@ -15,8 +15,8 @@ namespace AppointmentScheduler.Services
         public List<DoctorVM> GetDoctorList()
         {
             var doctors = (from user in _db.Users
-                           join userRoles in _db.UserRoles on user.Id equals userRoles.RoleId
-                           join roles in _db.Roles.Where(x=>x.Name==Helper.Doctor) on userRoles.RoleId equals roles.Id
+                           join userRoles in _db.UserRoles on user.Id equals userRoles.UserId
+                           join roles in _db.Roles.Where(x => x.Name == Helper.Doctor) on userRoles.RoleId equals roles.Id
                            select new DoctorVM
                            {
                                Id = user.Id,
@@ -28,7 +28,7 @@ namespace AppointmentScheduler.Services
         public List<PatientVM> GetPatientList()
         {
             var patients = (from user in _db.Users
-                           join userRoles in _db.UserRoles on user.Id equals userRoles.RoleId
+                           join userRoles in _db.UserRoles on user.Id equals userRoles.UserId
                            join roles in _db.Roles.Where(x => x.Name == Helper.Patient) on userRoles.RoleId equals roles.Id
                            select new PatientVM
                            {
